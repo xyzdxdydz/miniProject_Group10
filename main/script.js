b1fcda3b2f9ae522fa7f7d192a75d1ec40ab150b
xyzdxdydz <mastermind2012@hotmail.co.th>
2021-02-13 11:36:10 +0700

    Draft-Script

--- main/script.js
+++ main/script.js
@@ -1,6 +1,13 @@
-function makeNewNode(text) {
+function makeNewNode(text, status) {
     newNode = document.createElement('div');
-    newNode.setAttribute("class", "slot");
+    var assign;
+    if (status == 1) {
+        assign = "freeSlot";
+    } else {
+        assign = "inUseSlot";
+    }
+
+    newNode.setAttribute("class", assign);
     addText = document.createElement('h2');
     addText.innerText = text;
     newNode.appendChild(addText);
@@ -8,9 +15,10 @@ function makeNewNode(text) {
 }
 
 var i;
-
+var data = [1, 0, 0, 1];
 show = document.getElementById("statussection");
 
-for (i = 1; i < 4; i++) {
-    show.appendChild(makeNewNode("Slot" + i));
+for (i = 1; i <= 4; i++) {
+    show.appendChild(makeNewNode("Slot" + i, data[i-1]));
+    
 }
