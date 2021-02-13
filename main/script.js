09cb6217e2da8d0a9f12f12de2ddf7c758850e95
xyzdxdydz <mastermind2012@hotmail.co.th>
2021-02-13 13:29:51 +0700

    Add popup modal

--- main/script.js
+++ main/script.js
@@ -1,5 +1,7 @@
-function makeNewNode(text, status) {
-    newNode = document.createElement('div');
+
+// Function Section
+function makeNewNode(text, num, status) {
+    newNode = document.createElement('input');
 
     var assign;
     if (status == 1) {
@@ -9,16 +11,78 @@ function makeNewNode(text, status) {
     }
 
     newNode.setAttribute("class", assign);
+    newNode.setAttribute("id", i);
+    newNode.setAttribute("type", "submit");
+    newNode.setAttribute("value", text + i);
+    newNode.setAttribute("onClick", "reply_click(this.id)");
     addText = document.createElement('h2');
-    addText.innerText = text;
     newNode.appendChild(addText);
     return newNode;
 }
 
+function reply_click(clicked_id) {
+      clickID = clicked_id;
+  }
+
+function showAllStat(id) {
+    box = document.createElement('div');
+    box.setAttribute("id", "showstat");
+
+    text = document.createElement('p');
+    text.innerText = "Some text in the Modal..";
+    box.appendChild(text);
+
+    content.appendChild(box);
+}   
+
+// Variable Section
 var i;
 var data = [1, 0, 0, 1];
+
+// Get Data section
 show = document.getElementById("statussection");
+content = document.getElementById("forcontent");
 
 for (i = 1; i <= 4; i++) {
-    show.appendChild(makeNewNode("Slot " + i, data[i-1]));
+    show.appendChild(makeNewNode("Slot ", i, data[i-1]));
+}
+
+
+var btn1 = document.getElementById("1");
+var btn2 = document.getElementById("2");
+var btn3 = document.getElementById("3");
+var btn4 = document.getElementById("4");
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+modal.style.display = "block";
+showAllStat("1");
+}
+btn2.onclick = function() {
+modal.style.display = "block";
+showAllStat("2");
+}
+btn3.onclick = function() {
+modal.style.display = "block";
+showAllStat("3");
+}
+btn4.onclick = function() {
+modal.style.display = "block";
+showAllStat("4");
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+    var rm = document.getElementById("showstat");
+    rm.remove();
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    var rm = document.getElementById("showstat");
+    rm.remove();
+    }
 }
